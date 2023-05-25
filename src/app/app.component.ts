d7b654d35584a3829c9ba0c9ea8d01b37daa1c34
kadirchan <kadircanbozkurt1905@gmail.com>
2023-05-25 04:11:45 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+// import json file
+import data from './244820.json';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hw';
+  distantAddresses: Set<string> = new Set();
+  counter = 0;
+  size = 0;
+  func(): void {
+    this.counter = 0;
+    const jsonData = data.tx;
+    for (const tx of jsonData) {
+      if (tx.inputs !== undefined) {
+        for (const input of tx.inputs) {
+          if (input.prev_out !== undefined) {
+            if ('addr' in input.prev_out) {
+              this.distantAddresses.add(input.prev_out.addr);
+              this.counter++;
+            }
+          }
+        }
+      }
+      if (tx.out !== undefined) {
+        for (const output of tx.out) {
+          if ('addr' in output) {
+            this.distantAddresses.add(output.addr);
+            this.counter++;
+          }
+        }
+      }
+    }
+    console.log(this.distantAddresses);
+    this.size = this.distantAddresses.size;
+  }
+
+  hiddens(): void {}
 }
